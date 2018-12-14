5f773fe2ae05006f323df19e1cfb33bffe1585e5
dylansaxty <37349022+dylansaxty@users.noreply.github.com>
2018-12-13 20:53:41 -0500

    Create Button.js

---
+++ Button.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Javascript</title>
+  <script type = "text/javascript">
+    function funcClick() {
+      var x = document.getElementById("myPara");
+      x.innerHTML = "hello Niggas!";
+      }
+    </script>
+  </head>
+ <body>
+ <p id="mypara"> this is bogus</p>
+ <button type="button" onclick="funcClick()">Clicke Me</button>
+ </head>
+ </html>
